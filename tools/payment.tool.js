9022cbf0bb6ecc3d92917601a46d06f0e10877cc
Alex Ho <81464025+hoyipyik@users.noreply.github.com>
2023-08-03 16:22:51 +0800

    add payment functions

---
+++ tools/payment.tool.js
@@ -0,0 +1,39 @@
+// This file stores functions on payment
+const AlipaySdk = require('alipay-sdk').default;
+const config = require('../config');
+const { patch } = require('../users/users.model');
+const { setValue } = require('./cachemanager.tool');
+// TypeScript，可以使用 import AlipaySdk from 'alipay-sdk';
+// 普通公钥模式
+const alipaySdk = new AlipaySdk({
+  appId: config.app_id,
+  keyType: config.key_type, // 默认值。请与生成的密钥格式保持一致，参考平台配置一节
+  privateKey: config.private_key,
+  alipayPublicKey: config.alipay_public_key,
+});
+
+exports.paymentHandler = async (user, userId, addedCoinsNum, cacheKey) => {
+    const result = await alipaySdk.exec('alipay.open.public.qrcode.create');
+    console.log(result)
+    
+    console.log('money gotten')
+    // console.log(new Date().getSeconds())
+    await sleep(5);
+    // console.log(new Date().getSeconds())
+    await coinAdder(user, userId, addedCoinsNum)
+    await setValue(cacheKey, true)
+}
+
+const coinAdder = async (user, userId, addedCoinsNum) => {
+    const newCoins = user.coins + addedCoinsNum;
+    const upgradedData = {...user};
+    upgradedData.coins = newCoins;
+    // console.log(upgradedData);
+    const result = await patch(userId, upgradedData);
+    // console.log("coin add", result);
+    return {result, newCoins};
+}
+
+const sleep = async (second) => {
+    return new Promise(resolve => setTimeout(resolve, second * 1000));
+}
